099fb6fa84e028a3e999931c155fb6676cc34518
esraaJoumaa <esraa.2002.j1@gmail.com>
2025-06-01 17:42:46 +0300

    update admin.ts

--- queries/auth/admin.ts
+++ queries/auth/admin.ts
@@ -4,3 +4,11 @@ import type { userModel } from '~/models/userModel'
 export const useLoginAdmin = (user: userModel) => {
   return useAPI({ url: '/login', payload: user, queryKey: 'user', type: 'POST' })
 }
+
+export const useRegister = (user: userModel) => {
+  return useAPI({ url: '/register', payload: user, queryKey: 'user', type: 'POST' })
+}
+
+export const useRegugee = (user: userModel) => {
+  return useAPI({ url: '/refugees', payload: user, queryKey: 'user', type: 'POST' })
+}
