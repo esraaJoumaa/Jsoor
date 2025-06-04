b1f0da85ea7493d41eb22a26be797b66d50f64a7
esraaJoumaa <esraa.2002.j1@gmail.com>
2025-06-04 10:48:35 +0300

    create company

---
+++ pages/dashboard/company.vue
@@ -0,0 +1,90 @@
+<script setup lang="ts">
+const data = ref([
+  {
+    id: '4600',
+    date: '2024-03-11T15:30:00',
+    status: 'paid',
+    email: 'james.anderson@example.com',
+    amount: 594
+  },
+  {
+    id: '4599',
+    date: '2024-03-11T10:10:00',
+    status: 'failed',
+    email: 'mia.white@example.com',
+    amount: 276
+  },
+  {
+    id: '4598',
+    date: '2024-03-11T08:50:00',
+    status: 'refunded',
+    email: 'william.brown@example.com',
+    amount: 315
+  },
+  {
+    id: '4597',
+    date: '2024-03-10T19:45:00',
+    status: 'paid',
+    email: 'emma.davis@example.com',
+    amount: 529
+  },
+  {
+    id: '4596',
+    date: '2024-03-10T15:55:00',
+    status: 'paid',
+    email: 'ethan.harris@example.com',
+    amount: 639
+  }
+])
+const page=ref(0)
+</script>
+<template>
+   <div class="flex bg-[#F5F5F5] flex-col h-screen overflow-hidden">
+    <UDashboardNavbar class="bg-primary  shadow-sm">
+      <template #left>
+        <div class="flex items-center">
+          <span class="text-white font-semibold text-xl">Jsoor</span>
+        </div>
+      </template>
+      <template #right>
+        <div class="flex items-center gap-4">
+          <span class="text-white font-medium">Super Admin</span>
+          <UAvatar
+            src="https://github.com/benjamincanac.png"
+            size="md"
+          />
+        </div>
+      </template>
+    </UDashboardNavbar>
+    <div class="flex flex-1">
+      <UDashboardSidebar
+        resizable
+        class="bg-primary overflow-y-auto"
+      >
+        <DashboardSideBar />
+      </UDashboardSidebar>
+      <div class="flex-1 p-6">
+        <div class="flex justify-between items-center mb-4">
+          <h1 class="text-2xl text-primary font-bold pb-2">
+            Companies
+          </h1>
+        </div>
+        <div class="rounded-lg bg-white shadow-xl pb-5">
+          <UTable
+            ref="table"
+            :data="data"
+            sticky
+            class="flex-1 pt-0 max-h-[500px]"
+          />
+           <div class="flex justify-end border-t border-default pt-4 px-4">
+             <UPagination v-model:page="page"  />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
