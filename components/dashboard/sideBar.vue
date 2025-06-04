70743b0e945b61c22efe816d4e8a3ea5a737b03d
esraaJoumaa <esraa.2002.j1@gmail.com>
2025-06-04 10:49:21 +0300

    create sideBar

---
+++ components/dashboard/sideBar.vue
@@ -0,0 +1,47 @@
+<script lang="ts" setup>
+import type { NavigationMenuItem } from '@nuxt/ui'
+
+const items: NavigationMenuItem[] = [{
+  label: 'Users',
+  icon: 'i-lucide-users',
+  to: '/dashboard'
+},
+{
+  label: 'Companies',
+  icon: 'i-lucide-users',
+  to: '/dashboard/company'
+},
+]
+</script>
+
+<template>
+  <div>
+    <UNavigationMenu
+      :items="items"
+      class="text-Sequand"
+      orientation="vertical"
+      :ui="{
+        link: 'hover:text-primary text-lg py-4 px-4 transition-colors duration-200',
+        list: 'space-y-2',
+      }"
+    >
+      <template #item="itemValue">
+        <h1
+          class="hover:cursor-pointer"
+          :class="{ 'text-primary': itemValue.item.to===useRoute().fullPath }"
+        >
+          <span class="mt-5 text-xl px-2">
+            <Icon
+              :name="itemValue.item.icon"
+              class=""
+            />
+          </span>
+          {{ itemValue.item.label }}
+        </h1>
+      </template>
+    </UNavigationMenu>
+  </div>
+</template>
+
+<style>
+</style>
