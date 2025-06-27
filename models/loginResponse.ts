a3ba241d31d41169d86a55d8af60f133ea89ebe7
esraaJoumaa <esraa.2002.j1@gmail.com>
2025-06-27 19:47:44 +0300

    Create Model for LoginResponse

---
+++ models/loginResponse.ts
@@ -0,0 +1,13 @@
+export interface user{
+    user:{
+        id: string;
+        name : string;
+        email:string;
+        phone:string;
+        role:string;
+        type:string;
+        created_at:string;
+        updated_at:string;
+    },
+    token:string;
+}
