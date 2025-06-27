c15ed676c933f5dfe57d5300d83309c6dd02feee
esraaJoumaa <esraa.2002.j1@gmail.com>
2025-06-27 19:48:51 +0300

    Create Refgee Model

---
+++ models/refugeeModel.ts
@@ -0,0 +1,9 @@
+export interface refugeeModel {
+    name: string,
+  phone: string,
+  need: string,
+  number_of_family_member:string,
+  date_of_birth:string,
+  status:string,
+  user_id:string
+}
