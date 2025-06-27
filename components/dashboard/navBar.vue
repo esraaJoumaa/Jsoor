4b8be2eebbcdcdaac4c9eba2e4315ccfa7d607ca
esraaJoumaa <esraa.2002.j1@gmail.com>
2025-06-27 19:45:24 +0300

    Create Dashboard Navbar

--- components/dashboard/navBar.vue
+++ components/dashboard/navBar.vue
@@ -1,24 +1,25 @@
 <script setup lang="ts">
 
 </script>
+
 <template>
   <div>
     <UDashboardNavbar class="bg-[#288be2bd]">
-    <template #left>
-      <div class="flex items-centetr">
-        <span class="text-2xl font-bold text-white">Jsoor</span>
-      </div>
-    </template>
-    <template #right>
-      <div class="flex items-center gap-4">
+      <template #left>
+        <div class="flex items-centetr">
+          <span class="text-2xl font-bold text-white">Jsoor</span>
+        </div>
+      </template>
+      <template #right>
+        <div class="flex items-center gap-4">
           <span class="text-white font-medium">Super Admin</span>
           <UAvatar
             src="https://github.com/benjamincanac.png"
             size="md"
           />
         </div>
-    </template>
-  </UDashboardNavbar>
+      </template>
+    </UDashboardNavbar>
   </div>
 </template>
 
