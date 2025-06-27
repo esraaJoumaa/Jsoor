e7d3245a46db89f9f49a006388bfc20593be57df
esraaJoumaa <esraa.2002.j1@gmail.com>
2025-06-27 19:43:58 +0300

    Create Footer

--- components/appFooter.vue
+++ components/appFooter.vue
@@ -1,17 +1,23 @@
 <script setup lang="ts">
 </script>
 
-
 <template>
   <footer class="bg-gray-800 text-white py-8">
     <div class="container mx-auto px-4 text-center max-w-6xl">
-      <p>&copy; {{ new Date().getFullYear() }} Jasoor Project 
-        All rights reserved 
+      <p>
+        &copy; {{ new Date().getFullYear() }} Jasoor Project
+        All rights reserved
       </p>
       <div class="mt-4 flex justify-center space-x-6">
-        <a href="#" class="text-gray-400 hover:text-white transition-colors duration-300">Privacy Policy</a>
+        <a
+          href="#"
+          class="text-gray-400 hover:text-white transition-colors duration-300"
+        >Privacy Policy</a>
         <span class="text-gray-600">|</span>
-        <a href="#" class="text-gray-400 hover:text-white transition-colors duration-300">Terms of Use</a>
+        <a
+          href="#"
+          class="text-gray-400 hover:text-white transition-colors duration-300"
+        >Terms of Use</a>
       </div>
       <p class="text-gray-500 text-sm mt-2">
         "Jsoor"  A bridge of hope for a better future
@@ -19,7 +25,6 @@
     </div>
   </footer>
 </template>
-
 
 <style scoped>
 </style>
