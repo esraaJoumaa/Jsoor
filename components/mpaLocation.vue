b0bb107b01ab5b6e1a27ebff4653f8a44553b3aa
esraaJoumaa <esraa.2002.j1@gmail.com>
2025-06-27 19:45:05 +0300

    Add Map to Company Page

--- components/mpaLocation.vue
+++ components/mpaLocation.vue
@@ -1,52 +1,47 @@
 <script setup lang="ts">
-import 'leaflet/dist/leaflet.css';
+import 'leaflet/dist/leaflet.css'
 import type { Loction } from '@@/models/location'
 
-const emit = defineEmits(['location-selected']);
+const emit = defineEmits(['location-selected'])
 
 interface Props {
   initialLocation?: Loction | null;
 }
-const props = defineProps<Props>();
+const props = defineProps<Props>()
 
-const map = ref<any>(null);
-const markerPosition = ref<[number, number] | null>(null);
+const map = ref<any>(null)
+const markerPosition = ref<[number, number] | null>(null)
 
-const initialCenter = ref<[number, number]>([30.0444, 31.2357]);
+const initialCenter = ref<[number, number]>([33.5138, 36.2765])
 
+const sendLocationToApi = ref('')
 const handleMapClick = (event: any) => {
-  const lat = event.latlng.lat;
-  const lng = event.latlng.lng;
-  markerPosition.value = [lat, lng];
-  emit('location-selected', { lat, lng });
-};
+  const lat = event.latlng.lat
+  const lng = event.latlng.lng
+  markerPosition.value = [lat, lng]
+  sendLocationToApi.value = `${lat}|${lng}`
+  emit('location-selected', { lat, lng })
+}
 
 onMounted(() => {
   if (props.initialLocation) {
-    initialCenter.value = [props.initialLocation.lat, props.initialLocation.lng];
-    markerPosition.value = [props.initialLocation.lat, props.initialLocation.lng];
+    initialCenter.value = [props.initialLocation.lat, props.initialLocation.lng]
+    markerPosition.value = [props.initialLocation.lat, props.initialLocation.lng]
     if (map.value?.leafletObject) {
-      map.value.leafletObject.setView(initialCenter.value, 13);
+      map.value.leafletObject.setView(initialCenter.value, 13)
     }
   }
-});
-
-watch(() => props.initialLocation, (newLocation) => {
-  if (newLocation && map.value?.leafletObject) {
-    initialCenter.value = [newLocation.lat, newLocation.lng];
-    markerPosition.value = [newLocation.lat, newLocation.lng];
-    map.value.leafletObject.setView(initialCenter.value, 13);
-  }
-}, { deep: true });
+})
 </script>
+
 <template>
   <div class="">
     <LMap
       style="width: 100%; height: 200px;"
-      :zoom="13"
+      :zoom="60"
       :center="initialCenter"
-      @click="handleMapClick"
       :use-global-leaflet="false"
+      @click="handleMapClick"
     >
       <LTileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -54,11 +49,13 @@ watch(() => props.initialLocation, (newLocation) => {
         layer-type="base"
         name="OpenStreetMap"
       />
-      <LMarker v-if="markerPosition" :lat-lng="markerPosition" />
+      <LMarker
+        v-if="markerPosition"
+        :lat-lng="markerPosition"
+      />
     </LMap>
   </div>
 </template>
-
 
 <style scoped>
 </style>
