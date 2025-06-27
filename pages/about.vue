601b562ae41d0efc733eb22f1cfb36c51b9a6a1f
esraaJoumaa <esraa.2002.j1@gmail.com>
2025-06-27 19:49:24 +0300

    Create AboutUs Page

--- pages/about.vue
+++ pages/about.vue
@@ -1,7 +1,5 @@
-
 <script setup lang="ts">
 </script>
-
 
 <template>
   <div>
@@ -9,8 +7,12 @@
 
     <section class="bg-blue-50 py-16 text-center border-b border-gray-200">
       <div class="container mx-auto px-4 max-w-5xl">
-        <h1 class="text-4xl font-extrabold text-gray-900 mb-4">About the Jsoor Project</h1>
-        <p class="text-lg text-gray-700">Building bridges of hope for a better future</p>
+        <h1 class="text-4xl font-extrabold text-gray-900 mb-4">
+          About the Jsoor Project
+        </h1>
+        <p class="text-lg text-gray-700">
+          Building bridges of hope for a better future
+        </p>
       </div>
     </section>
 
@@ -20,12 +22,12 @@
           <p class="mb-6 leading-relaxed">
             Welcome to "Jsoor"
             a dedicated platform designed to bridge the gap for displaced individuals seeking to rebuild their lives
-             Our name "Jsoor" reflects our core mission: to connect displaced people with essential services and opportunities
+            Our name "Jsoor" reflects our core mission: to connect displaced people with essential services and opportunities
           </p>
           <p class="mb-6 leading-relaxed">
             At Jsoor
-             we understand the challenges faced by those seeking new beginnings
-             Our platform aims to provide vital support in key areas:
+            we understand the challenges faced by those seeking new beginnings
+            Our platform aims to provide vital support in key areas:
           </p>
           <ul class="list-disc list-inside mb-6 space-y-2">
             <li>
@@ -39,26 +41,31 @@
             </li>
           </ul>
 
-          <h2 class="text-3xl font-bold text-gray-900 mb-4 mt-8">How Jsoor Works?</h2>
+          <h2 class="text-3xl font-bold text-gray-900 mb-4 mt-8">
+            How Jsoor Works?
+          </h2>
 
-          <h3 class="text-2xl font-semibold text-gray-800 mb-2">For Displaced Individuals:</h3>
+          <h3 class="text-2xl font-semibold text-gray-800 mb-2">
+            For Displaced Individuals:
+          </h3>
           <p class="mb-6 leading-relaxed">
             Displaced individuals can register on our platform Once your application is reviewed and approved
             you'll gain full access to all available advertisements
             This allows you to browse and benefit from a wide range of services, including job postings, shelter options, and psychological support initiatives tailored to your needs.
           </p>
 
-          <h3 class="text-2xl font-semibold text-gray-800 mb-2">For Companies and Service Providers:</h3>
+          <h3 class="text-2xl font-semibold text-gray-800 mb-2">
+            For Companies and Service Providers:
+          </h3>
           <p class="mb-6 leading-relaxed">
             Companies and organizations can also register to become a part of the Jasoor network. Our platform offers a powerful way to display your services directly to those who need them most.
-             When posting advertisements, you have the ability to:
+            When posting advertisements, you have the ability to:
           </p>
           <ul class="list-disc list-inside mb-6 space-y-2">
             <li>Showcase your offerings in detail.</li>
             <li>Pinpoint your exact location for physical services.</li>
             <li>Specify service areas to ensure your help reaches the right people.</li>
           </ul>
-        
 
           <p class="text-lg font-semibold text-center mt-10">
             Our goal is to foster a supportive environment where displaced individuals can find the resources they need to thrive, and organizations can extend their reach to make a meaningful impact.
@@ -70,7 +77,6 @@
     <AppFooter />
   </div>
 </template>
-
 
 <style scoped>
 </style>
